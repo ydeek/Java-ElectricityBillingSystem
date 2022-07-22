a2dfaa99925a842515fb53f61ef7000c90e0c554
Yazan Deek <ydeek91@yahoo.com>
2022-07-22 15:13:47 -0700

    create connection to mysql database

--- ElectricityBillingSystem/src/main/java/revature/Electricity/Conn.java
+++ ElectricityBillingSystem/src/main/java/revature/Electricity/Conn.java
@@ -1,5 +1,21 @@
 package revature.Electricity;
 
+import java.sql.*;
+
 public class Conn {
+	Connection c;
+	Statement s; 
+	
+	public Conn() {
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			c =DriverManager.getConnection("jdbc:mysql://ebs","root","coding123");
+			s =c.createStatement();
+			
+			
+		}catch(Exception e) {
+			System.out.println(e);
+		}
+	}
 
 }
