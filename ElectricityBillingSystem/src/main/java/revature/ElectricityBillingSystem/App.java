238e3c2be1712d687c8e6582e6f56190e5b0f984
Yazan Deek <ydeek91@yahoo.com>
2022-07-22 11:05:11 -0700

    Maven project

---
+++ ElectricityBillingSystem/src/main/java/revature/ElectricityBillingSystem/App.java
@@ -0,0 +1,13 @@
+package revature.ElectricityBillingSystem;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
